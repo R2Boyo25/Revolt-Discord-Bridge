3646177096b7ad1b117f7c972ea44ce6f937b594
R2boyo25 <r2boyo25@gmail.com>
2022-10-12 19:07:04 -0500

    Hide logging & add webhooks/masquerades

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 import os
 import json
 import re
+import logging
 
 load_dotenv()
 
@@ -17,8 +18,8 @@ load_dotenv()
 loop = asyncio.new_event_loop()
 
 with open("map.json") as f:
-    channelmap = json.load(f)
-mapchannel = {}
+    channelmap: "Discord -> Revolt" = json.load(f)
+mapchannel: "Revolt -> Discord" = {}
 
 for pair in channelmap.items():
     mapchannel[pair[1]] = pair[0]
@@ -53,29 +54,55 @@ async def on_ready():
 async def on_message(message):
     if message.author.id == bot.user.id:
         return
+
+    # Discord -> Revolt
     if str(message.channel.id) in channelmap:
-        print(
-            f"Sending '{message.content}' to {channelmap[str(message.channel.id)]}")
+        if int(message.author.discriminator) == 0:
+            return
+
         content = message.content
+
         a = re.match("<@([0-9]*)>", content)
+
         if a:
             c = discord.utils.get(
                 guild.members, id=int(a.group(1)))
+
             content = re.sub("<@([0-9]*)>", "@" +
                              (c.nick or c.name), content)
+
         a = re.match("@(\S*)", content)
+
         if a:
+
             try:
                 b = utils.get(server.members, nickname=a.group(1))
+
             except:
                 b = None
+
             if not b:
                 b = utils.get(server.members, name=a.group(1))
+
             if b:
                 content = re.sub("@(\S)*", f"<@{b.id}>", content)
+
         if cc := "\n".join([attachment.url for attachment in message.attachments]):
             content += "\n" + cc
-        await client.get_channel(channelmap[str(message.channel.id)]).send(f"`{message.author.nick or message.author.name}` " + content)
+
+        print(
+            f"Sending '{message.content}' to {channelmap[str(message.channel.id)]}")
+
+        try:
+            masquerade = revolt.Masquerade(
+                name=(message.author.nick or message.author.name), avatar=message.author.avatar.url)
+
+            await client.get_channel(channelmap[str(message.channel.id)]).send(content, masquerade=masquerade)
+
+        except:
+            content = f"`{message.author.display_name or message.author.name}` " + content
+
+            await client.get_channel(channelmap[str(message.channel.id)]).send(content)
 
 
 class Client(revolt.Client):
@@ -83,36 +110,66 @@ class Client(revolt.Client):
         global server, ll
         print("Revolt is ready")
         server = self.get_server(revolts)
-        discord.utils.setup_logging()
+        discord.utils.setup_logging(level=logging.WARNING)
         await bot.start(discordt)
 
     async def on_message(self, message: revolt.Message):
         if message.author.id == self.user.id:
             return
+
         if message.content.startswith("-invite"):
             i = await guild.system_channel.create_invite(reason="Revolt", max_age=300)
             await message.channel.send(i.url)
             return
 
-        print(
-            f"Sending '{message.content}' to {mapchannel[message.channel.id]}")
-        content = message.content
-        a = re.match("<@([0-9a-zA-Z]*)>", content)
-        if a:
-            c = utils.get(
-                server.members, id=a.group(1))
-            content = re.sub("<@([0-9a-zA-Z]*)>", "@" +
-                             (c.nickname or c.name), content)
-        a = re.match("@(\S*)", content)
-        if a:
-            b = discord.utils.get(guild.members, nick=a.group(1))
-            if not b:
-                b = discord.utils.get(guild.members, name=a.group(1))
-            if b:
-                content = re.sub("@(\S)*", f"<@{b.id}>", content)
-        if cc := "\n".join([attachment.url for attachment in message.attachments]):
-            content += "\n" + cc
-        await bot.get_channel(int(mapchannel[message.channel.id])).send(f"`{message.author.nickname or message.author.name}` " + content)
+        # Revolt -> Discord
+        if message.channel.id in mapchannel:
+            content = message.content
+
+            a = re.match("<@([0-9a-zA-Z]*)>", content)
+
+            if a:
+                c = utils.get(
+                    server.members, id=a.group(1))
+
+                content = re.sub("<@([0-9a-zA-Z]*)>", "@" +
+                                 (c.nickname or c.name), content)
+
+            a = re.match("@(\S*)", content)
+
+            if a:
+                b = discord.utils.get(guild.members, nick=a.group(1))
+
+                if not b:
+                    b = discord.utils.get(guild.members, name=a.group(1))
+
+                if b:
+                    content = re.sub("@(\S)*", f"<@{b.id}>", content)
+
+            if cc := "\n".join([attachment.url for attachment in message.attachments]):
+                content += "\n" + cc
+
+            print(
+                f"Sending '{message.content}' to {mapchannel[message.channel.id]}")
+
+            try:
+
+                channel = bot.get_channel(int(mapchannel[message.channel.id]))
+
+                for _webhook in await channel.webhooks():
+                    if _webhook.name == "RevoltBridge":
+                        webhook = _webhook
+                else:
+                    webhook = await channel.create_webhook(name="RevoltBridge")
+
+                await webhook.send(
+                    content, username=message.author.nickname or message.author.name, avatar_url=message.author.avatar.url)
+
+            except:
+
+                content = f"`{message.author.nickname or message.author.name}` " + content
+
+                await bot.get_channel(int(mapchannel[message.channel.id])).send(content)
 
 
 async def main():
